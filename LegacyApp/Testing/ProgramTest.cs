a17993da5cba7227087d0374866b3d7f8dd58e72
diduk <diduk38pl@gmail.com>
2024-05-07 13:55:23 +0300

    Added c# Project

---
+++ LegacyApp/Testing/ProgramTest.cs
@@ -0,0 +1,71 @@
+﻿
+using System;
+using LegacyApp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+//Test Jednostkowy тестируют только наш код , внешние сервисы и базу данных не можем , за это отвечает уже интеграционные тесты 
+[TestClass]
+public class ProgramTest
+
+{
+    [TestMethod]
+    public void AddUser_ReturnFalseWhenFirstNameNullOrEmpty()
+    {
+        //Arrange (creating variables for testing)
+        var userService = new UserService();
+        
+        //Act (creating test , call some method)
+        var result = userService.AddUser("n", "Diduk", "s27736@pjwstk.edu.pl", DateTime.Parse("1982-03-21"), 1);
+        
+        Console.WriteLine(result);
+        
+        //Assert ,check result for our expectations
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void AddUser_ReturnFalseWhenBadEmail()
+    {
+        //Arrage
+        var userService = new UserService();
+        var email = "s51516516@.12jfr";
+        
+        //Act
+        var result = userService.AddUser("n", "Diduk", email, DateTime.Parse("1982-03-21"), 1);
+        Console.WriteLine(result);
+        
+        //Assert
+        Assert.IsTrue(result);
+    }
+
+
+    [TestMethod]
+    public void AddUser_ReturnFalseWhenUserOlder21()
+    {
+        
+        //Arrage
+        var userService = new UserService();
+        //Act
+        var result = userService.AddUser("n", "Diduk", "s27736@pjwstk.edu.pl", DateTime.Parse("2003-03-21"), 1);
+        Console.WriteLine(result);
+        //Assert
+        Assert.IsTrue(result);
+    }
+
+
+    [TestMethod]
+    public void AddUser_ReturnsTrueWhenVeryImportantClient()
+    {   
+        //Arrage
+        ClientRepository clientRepository = new ClientRepository();
+        var client = clientRepository.GetById(2).Type;
+        
+        //Act
+        Console.WriteLine(client);
+        
+        //Assert
+        Assert.AreEqual("VeryImportantClient",client);
+        
+    }
+    
+}
---
+++ LegacyApp/UserService.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace LegacyApp
+{
+    public class UserService
+    {
+
+        public bool IsNullFullName(string firstName,string lastName)
+        {
+            if (string.IsNullOrEmpty(firstName)||string.IsNullOrEmpty(lastName))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool ValidationEmail(string email)
+        {
+            if (!email.Contains("@") && !email.Contains("."))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsUserOlder21(DateTime dateOfBirth)
+        {
+            var now = DateTime.Now;
+            int age = now.Year - dateOfBirth.Year;
+            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
+
+            if (age < 21)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        protected User CreateUser(string firstName, string lastName, string email, DateTime dateOfBirth, Client client)
+        {
+            var user = new User
+            {
+                Client = client,
+                DateOfBirth = dateOfBirth,
+                EmailAddress = email,
+                FirstName = firstName,
+                LastName = lastName
+            };
+            
+            if (client.Type == "VeryImportantClient")
+            {
+                user.HasCreditLimit = false;
+            }
+            else
+            if (user.HasCreditLimit)
+            {
+                var userCreditService = new UserCreditService();
+                int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
+                if (user.Client == "ImportantClient")
+                {
+                    creditLimit *= 2;
+                }
+                user.CreditLimit = creditLimit;
+            }
+            return user;
+        }
+
+        
+        protected bool IsValidCreditLimit(User user)
+        {
+            return !user.HasCreditLimit || user.CreditLimit >= 500;
+        }
+        public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
+        {
+            if (IsNullFullName(firstName,lastName)) return false;
+            
+            if (ValidationEmail(email)) return false;
+
+            if (IsUserOlder21(dateOfBirth)) return false;
+
+            var clientRepository = new ClientRepository();
+            var client = clientRepository.GetById(clientId);
+
+            var user = CreateUser(firstName, lastName, email, dateOfBirth, client);
+            
+
+            if (!IsValidCreditLimit(user))
+                return false;
+
+            
+            UserDataAccess.AddUser(user);
+            return true;
+        }
+    }
+}
+
